7e32933fef8f2f622798645e0023b8a7a4f35ab9
ras1k <rasikabdullah@gmail.com>
2023-05-18 20:36:38 +0600

    register page added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            
+            <h2>Register</h2>
+            <form action="">
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <input type="password" name="" id="" placeholder='Password' />
+                <input type="submit" value="Register" />
+            </form>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './components/Main/Main.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
   },
 ]);
 
