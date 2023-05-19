0b2344eaa802a792659afd4eb98cffacccd3e353
ras1k <rasikabdullah@gmail.com>
2023-05-19 23:37:39 +0600

    onsubmit added

--- src/components/RegisterRbs/RegisterRbs.jsx
+++ src/components/RegisterRbs/RegisterRbs.jsx
@@ -3,27 +3,35 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 const RegisterRbs = () => {
+    const handleSubmit = event => {
+        event.preventDefault()
+        const email =(event.target.email.value);
+        const password =(event.target.password.value);
+        console.log(email, password)
+    }
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                {/* <Form.Text className="text-muted">
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" />
+                    {/* <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                 </Form.Text> */}
-            </Form.Group>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
     );
 };
 
