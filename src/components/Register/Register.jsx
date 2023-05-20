1f1ef91f2c68ac3c58ad58684bb9bd0b5aaddded
ras1k <rasikabdullah@gmail.com>
2023-05-20 17:53:36 +0600

    error and success message added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,12 +3,15 @@ import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 const Register = () => {
+    const [error, setError] = useState('')
     const [email, setEmail] = useState('')
+    const [success, setSuccess] = useState('')
     const auth = getAuth(app);
     
 
     const handleSubmit = (event) =>{
         event.preventDefault();
+        setSuccess('')
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password)
@@ -16,9 +19,11 @@ const Register = () => {
         .then(result=>{
             const loggedUser = result.user;
             console.log(loggedUser);
-
+            event.target.reset();
+            setSuccess('User created successfully')
         })
         .catch(error=>{
+            setError(error.message)
             console.log(error)
         })
     }
@@ -34,12 +39,14 @@ const Register = () => {
         <div>
             <h2>Register</h2>
             <form onSubmit={handleSubmit}>
-                <input type="email" onChange={handleEmailChange} name="email" id="email" placeholder='Your Email' />
+                <input type="email" onChange={handleEmailChange} required name="email" id="email" placeholder='Your Email' />
                 <br />
-                <input type="password" onBlur={handlePasswordBlur} name="password" id="password" placeholder='Password' />
+                <input type="password" onBlur={handlePasswordBlur} required name="password" id="password" placeholder='Password' />
                 <br />
                 <input type="submit" value="Register" />
             </form>
+            <p className='text-danger'>{error}</p>
+            <p className="text-success">{success}</p>
         </div>
     );
 };
